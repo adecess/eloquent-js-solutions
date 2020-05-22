c7e9bde1591a55f0cb2a5243b90f9388bbaf424b
adecess <axel.saliniere@gmail.com>
2020-05-22 12:23:19 -0400

    changed logs

--- 4.data-structures/sum-of-the-range.js
+++ 4.data-structures/sum-of-the-range.js
@@ -19,8 +19,5 @@ const sum = (arr) => {
 console.log(range(1, 10));
 console.log(sum(range(1, 10)));
 
-console.log(range(1, 10, 2));
-console.log(sum(range(1, 10, 2)));
-
 console.log(range(5, 2, -1));
 console.log(sum(range(5, 2, -1)));
