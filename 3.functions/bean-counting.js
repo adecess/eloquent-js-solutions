7527f224aecdf59fb00a626b5cc83dda56da7add
adecess <axel.saliniere@protonmail.com>
2020-06-04 23:51:12 -0400

    adding countChar

--- 3.functions/bean-counting.js
+++ 3.functions/bean-counting.js
@@ -9,3 +9,15 @@ const countBs = (str) => {
 }
 
 console.log(countBs("BbC"));
+
+const countChar = (str, char) => {
+    let count = 0;
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] === char) {
+            count++
+        };
+    }
+    return count;
+}
+
+console.log(countChar("kakkerlak", "k"));
